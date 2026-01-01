a7f6a05394d3af822e1d0b14f98945a017720f04
marc-go <goringm64@gmail.com>
2026-01-01 14:41:00 +0100

    Add invsee Command Logic

--- src/main/java/de/jama/core/commands/InvSeeCommand.java
+++ src/main/java/de/jama/core/commands/InvSeeCommand.java
@@ -6,7 +6,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.Inventory;
 
 public class InvSeeCommand implements CommandExecutor {
     private final Main plugin;
@@ -17,6 +19,8 @@ public class InvSeeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+
         if(!sender.hasPermission("core.admin.invsee")) {
             sender.sendMessage(" §cPermission denied!");
             return true;
@@ -26,6 +30,17 @@ public class InvSeeCommand implements CommandExecutor {
             sender.sendMessage(" §cUsage: /invsee <player>");
             return true;
         }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if(target == null) {
+            sender.sendMessage("§cPlayer not found!");
+            return true;
+        }
+
+        PlayerInventory player_inf = target.getInventory();
+
+        player.openInventory(player_inf);
 
         return true;
     }
